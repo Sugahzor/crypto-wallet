d56d7d5d1470fbd183deea69cebfab13f1b82d52
Iulia Voicu <iuliamvoicu@yahoo.com>
2021-04-26 20:13:24 +0300

    add wallet demo project

---
+++ src/main/java/org/sci/finalproj/DogeRulzApplication.java
@@ -0,0 +1,43 @@
+package org.sci.finalproj;
+
+import org.sci.finalproj.model.User;
+import org.sci.finalproj.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.util.Calendar;
+import java.sql.Date;
+
+@SpringBootApplication
+public class DogeRulzApplication implements CommandLineRunner {
+	@Autowired //injects it here
+	private UserService userService;
+
+	public static void main(String[] args) {
+		SpringApplication.run(DogeRulzApplication.class, args);
+	}
+
+	@Override
+	public void run(String... args) throws Exception {
+		actualRun();
+	}
+
+	private void actualRun() {
+		Date user1Birth =  new java.sql.Date(Calendar.getInstance().getTime().getTime());
+		User user1 = new User("Iulia", "Sugah", "abcde", user1Birth);
+		User user2 = new User("Iulia2", "Sugah2", "abcde", user1Birth);
+		User user3 = new User("Iulia3", "Sugah3", "abcde", user1Birth);
+
+		userService.register(user1);
+		userService.register(user2);
+		userService.register(user3);
+
+		User testUser = new User();
+		testUser.setUsername("Sugah");
+		testUser.setPassword("abcde");
+		System.out.println("Testing user login: " + userService.login(testUser));
+
+	}
+}
---
+++ src/main/java/org/sci/finalproj/repo/UserRepo.java
@@ -0,0 +1,45 @@
+package org.sci.finalproj.repo;
+
+import org.sci.finalproj.model.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface UserRepo extends CrudRepository<User, Long> {
+
+    // findBy => predefined syntax
+    public List<User> findByName(String name);
+    public User findByUserName(String userName);
+
+    @Query("SELECT u FROM User u WHERE u.userName = ?1 and u.name = ?2")
+    public User customQuery(String userName, String name);
+
+    @Query("SELECT u FROM User u WHERE u.userName = :#{#jmn.userName}")
+    public User customQuery1(@Param("jmn") User user);
+
+//    public boolean updateByName
+//    public boolean deleteByUserName
+
+//    public void createUser() {
+//
+//    }
+//
+//    public void updateUser() {
+//
+//    }
+//
+//    public void getUser() {
+//
+//    }
+//
+//    public void deleteUser() {
+//
+//    }
+
+
+
+}
