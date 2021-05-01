61f4fa2bc96c07071935c66c8f53cd07005313d7
Iulia Voicu <iuliamvoicu@yahoo.com>
2021-05-01 12:39:58 +0300

    fix repos

--- src/main/java/org/sci/finalproj/repo/AssetRepo.java
+++ src/main/java/org/sci/finalproj/repo/AssetRepo.java
@@ -1,9 +1,14 @@
 package org.sci.finalproj.repo;
 
+import org.sci.finalproj.model.Asset;
+import org.sci.finalproj.model.Transaction;
 import org.sci.finalproj.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface AssetRepo extends CrudRepository<User, Long> {
+public interface AssetRepo extends CrudRepository<Asset, Long> {
+    public Asset save(Asset asset);
+
+    public Asset findByAssetId(Long cryptoId);
 }
--- src/main/java/org/sci/finalproj/repo/TransactionRepo.java
+++ src/main/java/org/sci/finalproj/repo/TransactionRepo.java
@@ -1,9 +1,11 @@
 package org.sci.finalproj.repo;
 
+import org.sci.finalproj.model.Transaction;
 import org.sci.finalproj.model.User;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface TransactionRepo extends CrudRepository<User, Long> {
+public interface TransactionRepo extends CrudRepository<Transaction, Long> {
+    public Transaction save(Transaction transaction);
 }
