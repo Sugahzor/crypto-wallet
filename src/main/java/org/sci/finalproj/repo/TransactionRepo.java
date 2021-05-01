c0b694c3a5ad7631dd7e8238235ee885c04d3ca0
Iulia Voicu <iuliamvoicu@yahoo.com>
2021-05-01 12:50:01 +0300

    add TH query for specific date

--- src/main/java/org/sci/finalproj/repo/TransactionRepo.java
+++ src/main/java/org/sci/finalproj/repo/TransactionRepo.java
@@ -2,10 +2,17 @@ package org.sci.finalproj.repo;
 
 import org.sci.finalproj.model.Transaction;
 import org.sci.finalproj.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public interface TransactionRepo extends CrudRepository<Transaction, Long> {
     public Transaction save(Transaction transaction);
+
+    //attempt to query given a date selected by the user form interface
+    //eg: get TransactionHistory for userSelectedExecutionDate = 20.04.2021
+    @Query("SELECT u FROM Transaction u WHERE u.executionDate = :#{#userSelectedExecutionDate}")
+    public Transaction customQuery1(@Param("userSelectedExecutionDate") Transaction transaction);
 }
